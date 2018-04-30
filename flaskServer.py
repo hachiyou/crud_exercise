9b9c45f132b89466bf30fd1e847096b76a8cf001
Jianlin Feng <jfeng0083@yahoo.com>
2018-04-30 15:01:18 -0400

    Building the website now with FLASK framework.

---
+++ flaskServer.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify  # noqa
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+
+@app.route('/')
+def restaurantMenu():
+    """ Display the first restaurant's menu list. """
+    session = DBSession()  # Prevent Threading error.
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+    return output
+
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    """ Displaying a specific restaurant.
+
+    Arg:
+        restaurant_id (int): obtain from the URL builder
+                             as the id of the restaurant to look up
+    """
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+    return output
+
+
+
+if __name == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8080)
