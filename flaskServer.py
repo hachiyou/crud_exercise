46cc08f35c3ff6ead3500eaea702f3fe674899a0
Jianlin Feng <jfeng0083@yahoo.com>
2018-04-30 15:40:38 -0400

    Add functions for editing exisiting menu.

--- flaskServer.py
+++ flaskServer.py
@@ -14,7 +14,7 @@ DBSession = sessionmaker(bind=engine)
 @app.route('/')
 def restaurantMenu():
     """ Display the first restaurant's menu list. """
-    session = DBSession()  # Prevent Threading error.
+    session = DBSession()  # Prevent threading error.
     restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant=restaurant, items=items)
@@ -28,13 +28,14 @@ def restaurantMenu(restaurant_id):
         restaurant_id (int): obtain from the URL builder
                              as the id of the restaurant to look up
     """
-    session = DBSession()  # Prevent Threading error.
+    session = DBSession()  # Prevent threading error.
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 
-@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
+@app.route('/restaurants/<int:restaurant_id>/new',
+           methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     """ Create a new menu.
 
@@ -42,16 +43,44 @@ def newMenuItem(restaurant_id):
         restaurant_id (int): obtain from the URL builder
                              as the id of the restaurant to look up
     """
-    session = DBSession()  # Prevent Threading error.
+    session = DBSession()  # Prevent threading error.
     if request.method == 'POST':
-        newItem = MenuItem(name=request.form['name'], description=request.form[
-                           'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
+        newItem = MenuItem(name=request.form['name'],
+                           description=request.form['description'],
+                           price=request.form['price'],
+                           course=request.form['course'],
+                           restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
         flash("new menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    """ Edit a selected menu item.
+
+    Arg:
+        restaurant_id (int): obtain from the URL builder
+                             as the id of the restaurant to look up
+        menu_id (int): obtain from the URL builder
+                             as the id of the menu to look up
+    """
+    session = DBSession()  # Prevent threading error.
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        flash("menu item has been edited!")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
 
 if __name == '__main__':
